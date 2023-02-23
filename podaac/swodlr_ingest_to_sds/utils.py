361dd5087a42d3bf11060c5ed177b2a54a2618a7
Joshua Garde <jgarde@jpl.nasa.gov>
2023-02-22 16:51:58 -0800

    Linting + bumpver

--- podaac/swodlr_ingest_to_sds/utils.py
+++ podaac/swodlr_ingest_to_sds/utils.py
@@ -1,19 +1,22 @@
-import boto3
-from dotenv import load_dotenv
+'''Shared utilities for ingest-to-sds lambdas'''
 import sys
 from os import getenv
+import boto3
+from dotenv import load_dotenv
 from otello.mozart import Mozart
-from requests import Session
 
 load_dotenv()
 
 
 class Utils:
+    '''Utility functions implemented as a singleton'''
     APP_NAME = 'swodlr'
     SSM_PATH = f'/service/{APP_NAME}/ingest/'
 
     def __init__(self):
         self.env = getenv('SWODLR_ENV', 'prod')
+        self._mozart_client = None
+        self._ingest_table = None
 
         if self.env == 'prod':
             self._load_params_from_ssm()
@@ -31,13 +34,20 @@ class Utils:
             self._ssm_parameters[param['Name']] = param['Value']
 
     def get_param(self, name):
-        if (self.env == 'prod'):
+        '''
+        Retrieves a parameter from SSM or the environment depending on the
+        environment
+        '''
+        if self.env == 'prod':
             return self._ssm_parameters.get(name)
-        else:
-            return getenv(f'{self.APP_NAME.upper()}_{name}')
+
+        return getenv(f'{self.APP_NAME.upper()}_{name}')
 
     @property
     def mozart_client(self):
+        '''
+        Lazily creates a Mozart client
+        '''
         if not hasattr(self, '_mozart_client'):
             cfg = {
                 'host': self.get_param('sds_host'),
@@ -53,6 +63,9 @@ class Utils:
 
     @property
     def ingest_table(self):
+        '''
+        Lazily creates a DynamoDB table resource
+        '''
         if not hasattr(self, '_ingest_table'):
             dynamodb = boto3.resource('dynamodb')
             self._ingest_table = dynamodb.Table(
